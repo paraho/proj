216fbeb82cb5c843758279b925ef1461b518d6f0
jung.sn@gmail.com <abcde12#>
2016-04-24 15:05:05 +0900

    test

--- CF2DiffPatcher/CF2DiffPatcher/MainWindow.xaml.cs
+++ CF2DiffPatcher/CF2DiffPatcher/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CF2DiffPatcher
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
